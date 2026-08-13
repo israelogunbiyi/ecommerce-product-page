0865a454ea58cfb10a8b3032407bb5c87213a977
Israel-Cectar <ogunbiyiisrael99@gmail.com>
2026-08-13 16:44:12 +0100

    fourth commit

--- src/product.js
+++ src/product.js
@@ -1,7 +1,7 @@
-import img1 from "./images/image-product-1-thumbnail.jpg"
-import img2 from "./images/image-product-2-thumbnail.jpg"
-import img3 from "./images/image-product-3-thumbnail.jpg"
-import img4 from "./images/image-product-4-thumbnail.jpg"
+import img1 from "./images/image-product-1.jpg"
+import img2 from "./images/image-product-2.jpg"
+import img3 from "./images/image-product-3.jpg"
+import img4 from "./images/image-product-4.jpg"
 
 const initialProducts = [{
   id: crypto.randomUUID(),
